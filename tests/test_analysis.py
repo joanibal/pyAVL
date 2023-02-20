96caf452c6a6793d0918547594a443bdb90f4ccc
Josh Anibal <joanibal@umich.edu>
2023-02-20 09:52:00 -0800

    more formatting

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -16,37 +16,86 @@ import numpy as np
 
 base_dir = os.path.dirname(os.path.abspath(__file__))  # Path to current folder
 
-geom_file = os.path.join(base_dir, 'aircraft.txt')
-mass_file = os.path.join(base_dir, 'aircraft.mass')
+geom_file = os.path.join(base_dir, "aircraft.txt")
+mass_file = os.path.join(base_dir, "aircraft.mass")
 
 
 class TestAnalysisSweep(unittest.TestCase):
-    
     def setUp(self):
         self.avl_solver = AVLSolver(geo_file=geom_file, mass_file=mass_file)
-        
+
     def test_constrained_alpha_sweep(self):
-        self.avl_solver.addConstraint('D1', 0.00)
-        self.avl_solver.addConstraint('D2', 0.00)
-        
+        self.avl_solver.addConstraint("D1", 0.00)
+        self.avl_solver.addConstraint("D2", 0.00)
+
         self.avl_solver.alphaSweep(0, 10)
-        
-        np.testing.assert_allclose(self.avl_solver.CL, np.array([ 1.229212, 1.322154, 1.414586, 1.506469, 1.597764, 1.688435, 1.778445, 1.867757, 1.956337, 2.044148, 2.131159 ]) , rtol=1e-4)
-        np.testing.assert_allclose(self.avl_solver.CD, np.array([0.03059 , 0.033488, 0.036577, 0.039849, 0.043298, 0.046915,
-       0.050691, 0.054619, 0.058687, 0.062887, 0.067207]) , rtol=1e-4)
-        np.testing.assert_allclose(self.avl_solver.CM, np.zeros_like(self.avl_solver.CM), atol=1e-8 )
-    
+
+        np.testing.assert_allclose(
+            self.avl_solver.CL,
+            np.array(
+                [
+                    1.229212,
+                    1.322154,
+                    1.414586,
+                    1.506469,
+                    1.597764,
+                    1.688435,
+                    1.778445,
+                    1.867757,
+                    1.956337,
+                    2.044148,
+                    2.131159,
+                ]
+            ),
+            rtol=1e-4,
+        )
+        np.testing.assert_allclose(
+            self.avl_solver.CD,
+            np.array(
+                [
+                    0.03059,
+                    0.033488,
+                    0.036577,
+                    0.039849,
+                    0.043298,
+                    0.046915,
+                    0.050691,
+                    0.054619,
+                    0.058687,
+                    0.062887,
+                    0.067207,
+                ]
+            ),
+            rtol=1e-4,
+        )
+        np.testing.assert_allclose(self.avl_solver.CM, np.zeros_like(self.avl_solver.CM), atol=1e-8)
+
     def test_constrained_cl_sweep(self):
-        self.avl_solver.addConstraint('D1', 0.00)
-        self.avl_solver.addConstraint('D2', 0.00) 
+        self.avl_solver.addConstraint("D1", 0.00)
+        self.avl_solver.addConstraint("D2", 0.00)
         start_cl = 0.6
         end_cl = 1.6
         increment = 0.1
         self.avl_solver.CLSweep(start_cl, end_cl, increment)
-    
-        np.testing.assert_allclose(self.avl_solver.CL, np.arange(start_cl, end_cl + increment, increment) )
-        np.testing.assert_allclose(self.avl_solver.CD, np.array([0.016546, 0.018124, 0.019949, 0.022023, 0.024343, 0.02691, 0.029722, 0.032778, 0.036076, 0.039612, 0.043385]) , rtol=1e-4)
-        np.testing.assert_allclose(self.avl_solver.CM, np.zeros_like(self.avl_solver.CM) , atol=1e-8)
-        
 
-    
+        np.testing.assert_allclose(self.avl_solver.CL, np.arange(start_cl, end_cl + increment, increment))
+        np.testing.assert_allclose(
+            self.avl_solver.CD,
+            np.array(
+                [
+                    0.016546,
+                    0.018124,
+                    0.019949,
+                    0.022023,
+                    0.024343,
+                    0.02691,
+                    0.029722,
+                    0.032778,
+                    0.036076,
+                    0.039612,
+                    0.043385,
+                ]
+            ),
+            rtol=1e-4,
+        )
+        np.testing.assert_allclose(self.avl_solver.CM, np.zeros_like(self.avl_solver.CM), atol=1e-8)
--- tests/test_contraints.py
+++ tests/test_contraints.py
@@ -18,34 +18,35 @@ base_dir = os.path.dirname(os.path.abspath(__file__))  # Path to current folder
 
 # case = pyAVL.avlAnalysis(geo_file='aircraft.txt', mass_file ='aircraft.mass')
 
-aircraft_dir = os.path.join(base_dir, '../examples/aircraft')
-geom_file = os.path.join(base_dir, 'aircraft.txt')
-mass_file = os.path.join(base_dir, 'aircraft.mass')
+aircraft_dir = os.path.join(base_dir, "../examples/aircraft")
+geom_file = os.path.join(base_dir, "aircraft.txt")
+mass_file = os.path.join(base_dir, "aircraft.mass")
+
 
 class TestConstraints(unittest.TestCase):
     # TODO: add reference values for comparison by running avl binnary by hand or with wrappper
-    
+
     def setUp(self):
         self.avl_solver = AVLSolver(geo_file=geom_file, mass_file=mass_file)
-        
+
     # TODO: add test for roll, pitch, and yaw rate
     # def test_rates(self):
-    
-    
+
     def test_angles(self):
-        self.avl_solver.addConstraint('alpha', 6.00)
-        self.avl_solver.addConstraint('beta', 2.00)
+        self.avl_solver.addConstraint("alpha", 6.00)
+        self.avl_solver.addConstraint("beta", 2.00)
         self.avl_solver.executeRun()
-    
+
     def test_control_surfaces(self):
-        self.avl_solver.addConstraint('D1', 0.00)
-        self.avl_solver.addConstraint('D2', 0.00)
+        self.avl_solver.addConstraint("D1", 0.00)
+        self.avl_solver.addConstraint("D2", 0.00)
         self.avl_solver.executeRun()
-    
+
     def test_trim(self):
         # TODO: parametrize for the other options
-        self.avl_solver.addTrimCondition('CL', 1.0)
+        self.avl_solver.addTrimCondition("CL", 1.0)
         self.avl_solver.executeRun()
-    
+
+
 if __name__ == "__main__":
     unittest.main()
