ac6626c55ac0112f08bcd52de92776150cf62798
Josh Anibal <joanibal@umich.edu>
2023-03-17 16:15:38 -0700

    change version location

--- pyavl/__init__.py
+++ pyavl/__init__.py
@@ -1,3 +1,9 @@
-__version__ = "1.1.0"
+import importlib.metadata
+
+try:
+    # __package__ allows for the case where __name__ is "__main__"
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
 
 from .pyAVL import AVLSolver
