a3c02b87fb13d095d24449d626f3849845b8b686
Josh Anibal <joanibal@umich.edu>
2023-06-06 19:15:56 -0700

    fixed/genearlzed meson build

---
+++ src/grab-all-fortran-files.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+# # Standard Python modules
+import os
+# from pathlib import Path
+
+# CURDIR = os.path.abspath(os.path.dirname(__file__))
+
+# TO_SKIP = [
+#     "sn27lu77.f",
+#     "sn27lu90.f",
+#     "snopth.f",
+# ]
+
+# for path in Path(CURDIR).glob("*.f"):
+#     if os.path.basename(path) not in TO_SKIP:
+#         print(path)
+        
+def parse_file(file_name):
+    # Initialize lists for each file type
+    fortranFiles = []
+    f77Files = []
+    cFiles = []
+
+    # Define a dictionary to map identifiers to lists
+    list_map = {
+        "fortranFiles =": fortranFiles,
+        "f77Files =": f77Files,
+        "cFiles =": cFiles,
+    }
+
+    # Initialize current list as None
+    current_list = None
+
+    # Open and read the file line by line
+    with open(file_name, "r") as file:
+        for line in file:
+            # Remove leading/trailing white space
+            line = line.strip()
+            # Skip comments and empty lines
+            if line.startswith("#") or not line:
+                continue
+            # Check if the line is a new list identifier
+            new_list = False
+            for key in list_map:
+                if line.startswith(key):
+                    current_list = list_map[key]
+                    new_list = True
+                    break
+                
+                
+            # Otherwise, if a list is currently being filled, add the file to the list
+            if current_list is not None and not new_list:
+                # trim whitespace and trailing \
+                line = line.strip()
+                if line.endswith("\\"):
+                    line = line[:-1]
+                # prepend src/ to the file name
+                line = "src/" + line
+                
+                
+                # add to list
+                current_list.append(line)
+
+    # Print each file list
+    # print("fortranFiles = ", fortranFiles)
+    # print("f77Files = ", f77Files)
+    # print("cFiles = ", cFiles)
+    for file in f77Files:
+        print(file)
+    for file in cFiles:
+        print(file)
+
+# Call the function with the file name
+parse_file("src/build/fileList")  # replace "filename.txt" with the name of your file
