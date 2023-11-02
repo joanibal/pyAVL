44955b7d4204f742dd3e1262e841766b2e62b4db
Joshua Anibal <joshua.l.anibal@nasa.gov>
2023-11-02 06:07:35 -0700

    add back in all tests

--- tests/test_tear_down.py
+++ tests/test_tear_down.py
@@ -23,18 +23,15 @@ mass_file = os.path.join(base_dir, "aircraft.mass")
 geom_mod_file = os.path.join(base_dir, "aircraft_mod.avl")
 
 
-avl_solver = AVLSolver(geo_file=geom_file)
 class TestCaseDerivs(unittest.TestCase):
     def setUp(self) -> None:
-        # self.avl_solver = AVLSolver(geo_file=geom_file)
+        self.avl_solver = AVLSolver(geo_file=geom_file)
         self.avl_solver = avl_solver
         pass
     
     def tearDown(self):
         # Without the following line a copy of large_list will be kept in
         # memory for each test that runs, uncomment the line to allow the
-        # large_list to be garbage collected.
-        # self.large_list = None
         mb_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1000
         print("Memory usage: %s MB" % mb_memory)
 
--- tools/wheels/cibw_test_command.sh
+++ tools/wheels/cibw_test_command.sh
@@ -6,7 +6,21 @@ cd $PROJECT_DIR/tests
 # python -m unittest -v
 #HACK: if the tests are not split up the CI runs out of memory...
 
-python -m unittest -v test_analysis.py  &&python -m unittest -v test_io.py     &&
-python -m unittest -v test_surf_geom.py &&
-python -m unittest -v test_contraints.py  &&python -m unittest -v test_import.py      &&
+# test package built and installed correctly
+python -m unittest -v test_import.py
+python -m unittest -v test_io.py
+
+# test basic avl functionality
 python -m unittest -v test_parameters.py
+python -m unittest -v test_analysis.py
+python -m unittest -v test_surf_geom.py
+python -m unittest -v test_contraints.py
+
+# tests for adjoint
+python -m unittest -v test_new_subroutines.py
+python -m unittest -v test_partial_derivs.py
+python -m unittest -v test_total_derivs.py
+
+# test mem ussage of pyavl and test framework
+python -m unittest -v test_tear_down.py
+
