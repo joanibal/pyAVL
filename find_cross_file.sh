cc290a7e8bb1b3af3a83a491ceb7a6f3b62f8b48
Joshua Anibal <joanibal@umich.edu>
2024-09-08 20:52:51 -0700

    trying to debug eigen test 2 and some cleaning

--- find_cross_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Find the .ini file in the specified directory pattern
-file_path=$(find /Users/runner/work/pyAVL/pyAVL -name 'meson-python-cross-file.ini' -print -quit)
-
-# Output the found file path
-echo $file_path
