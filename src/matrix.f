a12d1cb54f362d6aa19aaed248c73638bfc8d886
Joshua Anibal <joanib14@gmail.com>
2025-01-10 17:40:39 -0800

    basic win build working?

--- src/matrix.f
+++ src/matrix.f
@@ -121,3 +121,36 @@ C
       RETURN
       END ! BAKSUB
 
+      SUBROUTINE BAKSUBTRANS(NSIZ,N,A,INDX,B)
+      REAL A(NSIZ,NSIZ), B(NSIZ)
+      INTEGER INDX(NSIZ)
+C
+      II = 0
+      DO 12 I=1, N
+        LL = INDX(I)
+        SUM = B(LL)
+        B(LL) = B(I)
+        IF(II.NE.0) THEN
+          DO 11 J=II, I-1
+            SUM = SUM - A(I,J)*B(J)
+   11    CONTINUE
+        ELSE IF(SUM.NE.0.0) THEN
+          II = I
+        ENDIF
+        B(I) = SUM
+   12 CONTINUE
+C
+      DO 14 I=N, 1, -1
+        SUM = B(I)
+        IF(I.LT.N) THEN
+          DO 13 J=I+1, N
+            SUM = SUM - A(I,J)*B(J)
+   13    CONTINUE
+        ENDIF
+        B(I) = SUM/A(I,I)
+   14 CONTINUE
+C
+      RETURN
+      END ! BAKSUBTRANS
+
+  
