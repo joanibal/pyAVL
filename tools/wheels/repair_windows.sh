816f48c4000332cf590af82065e0f0687f8881f6
Joshua Anibal <joanib14@gmail.com>
2025-01-10 20:20:54 -0800

    new repair wheel script

--- tools/wheels/repair_windows.sh
+++ tools/wheels/repair_windows.sh
@@ -9,7 +9,7 @@ pushd $DEST_DIR
 mkdir -p tmp
 pushd tmp
 wheel unpack $WHEEL
-pushd pyavl*
+pushd optvl*
 
 # To avoid DLL hell, the file name of libopenblas that's being vendored with
 # the wheel has to be name-mangled. delvewheel is unable to name-mangle PYD
@@ -17,7 +17,7 @@ pushd pyavl*
 # building with mingw.
 # We therefore find each PYD in the directory structure and strip them.
 
-for f in $(find ./pyavl* -name '*.pyd'); do strip $f; done
+for f in $(find ./optvl* -name '*.pyd'); do strip $f; done
 
 
 # now repack the wheel and overwrite the original
