7bf0e1be7e9511b6221ec860ac99f3d022309265
josh <joanibal@umich.edu>
2025-01-15 21:36:18 -0800

    try nvmax 6000

--- src/create_avl_inc.py
+++ src/create_avl_inc.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     elif platform.system() == "Linux":
         nvmax = 6000
     elif platform.system() == "Windows":
-        nvmax = 4500
+        nvmax = 6000
     else:
         raise NotImplementedError('system platform not found')
     
