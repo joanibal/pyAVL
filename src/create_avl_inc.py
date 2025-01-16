3c6356b657329b5398e9c42865351104b8e57f5b
josh <joanibal@umich.edu>
2025-01-15 21:52:34 -0800

    5000

--- src/create_avl_inc.py
+++ src/create_avl_inc.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     elif platform.system() == "Linux":
         nvmax = 6000
     elif platform.system() == "Windows":
-        nvmax = 5000
+        nvmax = 5500
     else:
         raise NotImplementedError('system platform not found')
     
