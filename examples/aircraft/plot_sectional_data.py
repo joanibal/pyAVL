6e55c50f13a4be9889c023c645a047d3481aa980
Joshua Anibal <joanib14@gmail.com>
2025-01-30 19:21:13 -0800

    tweaked plot sectional script

--- examples/aircraft/plot_sectional_data.py
+++ examples/aircraft/plot_sectional_data.py
@@ -9,8 +9,11 @@ ovl.execute_run()
 strip_data = ovl.get_strip_data()
 for surf_key in strip_data:
     span_distance = strip_data[surf_key]['XYZ LE'][:,1]
-    plt.plot(span_distance, strip_data[surf_key]['lift dist'])
+    plt.plot(span_distance, strip_data[surf_key]['lift dist'], color='blue')
+    plt.plot(span_distance, strip_data[surf_key]['CL'], color='red')
+    plt.plot(span_distance, strip_data[surf_key]['CL perp'], color='firebrick', linestyle='--')
 
+plt.legend(['lift dist', 'CL', 'CL perp.'])
 plt.ylabel('lift distribution')
 plt.xlabel('spanwise position')
 plt.show()
